d14d5e2ddd9c093b65c66a452a88827d6b1b009c
Giovanni Burresi <giovanniburresi@gmail.com>
2019-10-23 14:10:12 +0200

    Fixings
    MAca da creare iljson

--- test01.py
+++ test01.py
@@ -24,13 +24,29 @@ SERVER_NAME   = 'OPC.SimaticNET'
 OPC_NAME_ROOT = 'S7:[Collegamento_IM151_8]'
 
 
+
+GENERALE = 0
+GRUPPO_1 = 1
+GRUPPO_2 = 2
+GRUPPO_3 = 3
+
+FASI = 0
+INGRESSI = 1
+ALLARMI = 2
+
+NOME = 0
+INDIRIZZO = 1
+TIPO = 2
+NOTE = 3
+
+
 def readSingleData(variableName):
 	val = None
 
 	try:
 		value, quality, time = opc.read( OPC_NAME_ROOT + 'variableName' )
 	except OpenOPC.TimeoutError:
-		print "TimeoutError occured"
+		print "TimeoutError occured: IL PLC E SPENTO!!!"
 
 	return val
 
@@ -41,7 +57,7 @@ def readGroupData(varGroup):
 	try:
 		val = opc.read( varGroup )
 	except OpenOPC.TimeoutError:
-		print "TimeoutError occured"
+		print "TimeoutError occured: IL PLC E SPENTO!!!"
 
 	return val
 
@@ -58,11 +74,26 @@ available_servers = opc.servers()
 opc.connect( SERVER_NAME )
 
 ## read a GROUP of variable - opcGrops e' un array di stringhe
-valuesGrouped = readGroupData(varGroup)
+res = readGroupData(opcGroups)
 
-## prova a leggere i singoli a parte
-try:
-	for var in valuesGrouped:
-		print( len(value) )
-except OpenOPC.TimeoutError:
-	print "TimeoutError occured"
+labels = GelliBelloi.ALL_VARS_REORDERED
+
+## qui e' come prendo le label ma vogli farlo a classi
+print( GelliBelloi.Labels[GENERALE] )
+
+print( GelliBelloi.Labels[GRUPPO_1][FASI] )
+print( GelliBelloi.Labels[GRUPPO_1][INGRESSI] )
+print( GelliBelloi.Labels[GRUPPO_1][ALLARMI] )
+
+print( GelliBelloi.Labels[GRUPPO_2][FASI] )
+print( GelliBelloi.Labels[GRUPPO_2][INGRESSI] )
+print( GelliBelloi.Labels[GRUPPO_2][ALLARMI] )
+
+print( GelliBelloi.Labels[GRUPPO_3][FASI] )
+print( GelliBelloi.Labels[GRUPPO_3][INGRESSI] )
+print( GelliBelloi.Labels[GRUPPO_3][ALLARMI] )
+
+## stampo le dimensioni dei
+if res != None:
+    for var in res:
+        print( len(value) )
