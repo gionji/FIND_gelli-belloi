91ff78d483ad26f5f2bc4e0e42599fd7b81de723
Giovanni Burresi <giovanniburresi@gmail.com>
2019-10-23 18:31:40 +0200

    PRontivia!

--- test01.py
+++ test01.py
@@ -11,6 +11,8 @@ import datetime
 import re, time, csv
 import OpenOPC
 
+import json
+
 import GelliBelloi
 
 try:
@@ -24,21 +26,13 @@ SERVER_NAME   = 'OPC.SimaticNET'
 OPC_NAME_ROOT = 'S7:[Collegamento_IM151_8]'
 
 
-
-GENERALE = 0
-GRUPPO_1 = 1
-GRUPPO_2 = 2
-GRUPPO_3 = 3
-
-FASI = 0
-INGRESSI = 1
-ALLARMI = 2
-
 NOME = 0
 INDIRIZZO = 1
 TIPO = 2
 NOTE = 3
+LABEL = 4
 
+LABEL = INDIRIZZO
 
 def readSingleData(variableName):
 	val = None
@@ -60,6 +54,33 @@ def readGroupData(varGroup):
 		print "TimeoutError occured: IL PLC E SPENTO!!!"
 
 	return val
+    
+    
+    
+    
+def createJson(*elements):
+    callerInfo = {
+        "culture": "it-IT",
+        "timezone": "+01:00",
+        "version": "1.0.0" } 
+
+    output = {}
+
+    for couple in elements:
+        if isinstance(couple,tuple) and len(couple) == 2 and len(couple[0]) == len(couple[1]):
+            output.update( dict( zip( couple[0], couple[1] ) ) )
+        else:
+            print('Problem with data, impossible to create json')
+
+    res = {'output' : output, 'callerInfo' : callerInfo}
+    
+    return json.dumps(res)
+
+
+def sendJson():
+    print("Sending json")
+    print("DONE")
+    
 
 # Chose the groups of vars
 opcGroups = GelliBelloi.VAR_GROUPS_SUPER_COMPACT
@@ -74,26 +95,27 @@ available_servers = opc.servers()
 opc.connect( SERVER_NAME )
 
 ## read a GROUP of variable - opcGrops e' un array di stringhe
-res = readGroupData(opcGroups)
+res = readGroupData( opcGroups )
 
-labels = GelliBelloi.ALL_VARS_REORDERED
-
-## qui e' come prendo le label ma vogli farlo a classi
-print( GelliBelloi.Labels[GENERALE] )
-
-print( GelliBelloi.Labels[GRUPPO_1][FASI] )
-print( GelliBelloi.Labels[GRUPPO_1][INGRESSI] )
-print( GelliBelloi.Labels[GRUPPO_1][ALLARMI] )
-
-print( GelliBelloi.Labels[GRUPPO_2][FASI] )
-print( GelliBelloi.Labels[GRUPPO_2][INGRESSI] )
-print( GelliBelloi.Labels[GRUPPO_2][ALLARMI] )
-
-print( GelliBelloi.Labels[GRUPPO_3][FASI] )
-print( GelliBelloi.Labels[GRUPPO_3][INGRESSI] )
-print( GelliBelloi.Labels[GRUPPO_3][ALLARMI] )
 
 ## stampo le dimensioni dei
 if res != None:
-    for var in res:
-        print( len(value) )
+    jjj = createJson( 
+                (res[0], GelliBelloi.LabelsClass.Generale[ LABEL ] ),
+                (res[1], GelliBelloi.LabelsClass.Gruppo1.Fasi[ LABEL ] ),
+                (res[2], GelliBelloi.LabelsClass.Gruppo1.Ingressi[ LABEL ] ),
+                (res[3], GelliBelloi.LabelsClass.Gruppo1.Allarmi[ LABEL ] ),
+                (res[4], GelliBelloi.LabelsClass.Gruppo2.Fasi[ LABEL ] ),
+                (res[5], GelliBelloi.LabelsClass.Gruppo2.Ingressi[ LABEL ] ),
+                (res[6], GelliBelloi.LabelsClass.Gruppo2.Allarmi[ LABEL ] ),
+                (res[7], GelliBelloi.LabelsClass.Gruppo3.Fasi[ LABEL ] ),
+                (res[8], GelliBelloi.LabelsClass.Gruppo3.Ingressi[ LABEL ] ),
+                (res[9], GelliBelloi.LabelsClass.Gruppo3.Allarmi[ LABEL ] )
+                )
+
+    print( jjj )
+
+    sendJson()
+
+else:
+    print('cicciia! Nessun risultato, PLC spento')
